7148f915148bab31d2558a1e26d87076a7085b3e
Vimal Earnest <v_earnest@yahoo.com>
2017-02-17 15:19:44 -0800

    Added ag

--- config/projectile.el
+++ config/projectile.el
@@ -1,7 +1,11 @@
 (live-add-pack-lib "helm-projectile")
+(live-add-pack-lib "emacs-helm-ag")
 (live-add-pack-lib "projectile")
+(live-add-pack-lib "ag")
 (require 'projectile)
 (require 'helm-projectile)
+(require 'ag)
+(require 'helm-ag)
 
 (projectile-global-mode)
 
